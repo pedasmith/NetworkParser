bb89c03707885807d9d7c2037b302b6643e8a41b
Peter Smith <pedasmith@yahoo.com>
2019-03-30 19:22:01 -0700

    Added methods to convert byte[] and IBuffer into strings using either Utf8 or Latin1 chars.

---
+++ NetworkParsers/NetworkParsers/CharacterSets.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Security.Cryptography;
+using Windows.Storage.Streams;
+
+namespace NetworkParsers
+{
+    namespace Foundation
+    {
+        /// <summary>
+        /// Handles common problems when dealing with text-based internet protocols.
+        /// </summary>
+        public class CharacterSetConverters
+        {
+            static Encoding Utf8Encoder = new UTF8Encoding(false, true); // No BOM and throw on error
+            static Encoding Utf8EncoderNonThrowing = new UTF8Encoding(false, false); // No BOM and do not throw on error
+
+            /// <summary>
+            /// Converts a byte array in correct Utf8 format to a string. Non-UTF8 returns false.
+            /// </summary>
+            /// <param name="data">byte array with input data</param>
+            /// <param name="str">returned string. If return is true, guaranteed to be non-null.</param>
+            /// <returns>true if the original data is utf8. Null and empty are both considered utf8 </returns>
+            public static bool TryConvertUtf8ByteArrayToString(byte[] data, out string str)
+            {
+                // null data isn't not utf8.
+                if (data == null)
+                {
+                    str = "";
+                    return true;
+                }
+
+                bool isUtf8 = NetworkParsers.Utf8.IsUtf8(data, data.Length);
+                if (isUtf8)
+                {
+                    var unicodeString = "";
+                    try
+                    {
+                        unicodeString = Utf8Encoder.GetString(data);
+                        str = unicodeString ?? ""; // never null.
+                        return true;
+                    }
+                    catch (Exception)
+                    {
+                        // failed to decode. Our utf8 guess must have been wrong, which is essentially inconceivable.
+                        str = null;
+                        //return false;
+                    }
+
+                    // Try the other decoder. I don't know that it ever converts when the Utf8Encoder doesn't work.
+                    string cryptoString = "";
+                    try
+                    {
+                        IBuffer buffer = CryptographicBuffer.CreateFromByteArray(data);
+                        cryptoString = CryptographicBuffer.ConvertBinaryToString(BinaryStringEncoding.Utf8, buffer);
+                        str = cryptoString ?? "";
+                        return true;
+                    }
+                    catch (Exception)
+                    {
+                        ; // also fails
+                    }
+
+                    unicodeString = "";
+                    try
+                    {
+                        unicodeString = Utf8EncoderNonThrowing.GetString(data);
+                        str = unicodeString ?? ""; // never null.
+                        return true;
+                    }
+                    catch (Exception)
+                    {
+                        // more failures? How? Really just give up now.
+                    }
+                    // note that this is a total failure of the IsUtf8 promise.
+                    str = null;
+                    return false;
+                }
+
+                // Not utf8. 
+                str = null;
+                return false;
+            }
+
+            /// <summary>
+            /// Converts a byte array in correct Latin1 format to a string. Non-Latin1 returns false. Note that Latin1 includes a transcription for every byte!
+            /// </summary>
+            /// <param name="data">byte array with input data</param>
+            /// <param name="str">returned string. If return is true, guaranteed to be non-null.</param>
+            /// <returns>true if the original data is utf8. Null and empty are both considered utf8 </returns>
+            public static bool TryConvertLatin1ByteArrayToString(byte[] data, out string str)
+            {
+                // null data isn't not Latin1.
+                if (data == null)
+                {
+                    str = "";
+                    return true;
+                }
+
+                try
+                {
+                    var results = Encoding.GetEncoding(28591).GetString(data);
+                    str = results ?? "";
+                    return true;
+                }
+                catch (Exception)
+                {
+                    ; // failed to decode...
+                }
+
+                // Not utf8. 
+                str = null;
+                return false;
+            }
+
+            /// <summary>
+            /// Converts a byte array in correct ASCII format to a string. Non-ASCII chars are converted to a HEX output.
+            /// /// </summary>
+            /// <param name="data">byte array with input data</param>
+            /// <param name="str">returned string. Will always be the some representation of the string</param>
+            /// <returns>true if the original data is ASCII. Null and empty are both considered utf8 </returns>
+            public static bool TryConvertASCIIByteArrayToString(byte[] data, out string str)
+            {
+                // null data isn't not Latin1.
+                if (data == null)
+                {
+                    str = "";
+                    return true;
+                }
+                bool retval = true;
+
+                // Try the giant hammer of decoding. This should never be called because
+                // the 28591 encoding can convert any byte!
+                var sb = new StringBuilder();
+                foreach (var ch in data)
+                {
+                    if (ch >= 128)
+                    {
+                        retval = false;
+                        sb.Append($"[{ch:X}]");
+                    }
+                    else
+                    {
+                        sb.Append((char)ch);
+                    }
+                }
+                str = sb.ToString();
+                return retval;
+            }
+        }// End of the Foundation namespace.
+
+        /// <summary>
+        /// Simple statics method to convert byte arrays into strings, guessing at the correct encoding. 
+        /// In all cases, a string will be returned.
+        /// Will attempt to decode the string as UTF8 or LATIN1. In the impossible case that the LAIN1 converter doesn't work,
+        /// will convert as ASCII, writing out bytes with the top bit on as [HEX].
+        /// In the case of improper UTF8 (like MUTF-8's overlong "C080"--> "\0" conversion),
+        /// the output will replace the C0 and 80 with � characters.
+        /// </summary>
+        public class CharacterSets
+        {
+            /// <summary>
+            /// Converts a byte[] array into a string with either a UTF8 (preferred) or Latin-1 character set.
+            /// </summary>
+            /// <param name="data">byte[] of data</param>
+            /// <returns>non-null string in all cases (even if data is null)</returns>
+            public static string ConvertByteArrayToString(byte[] data)
+            {
+                bool okUtf8 = Foundation.CharacterSetConverters.TryConvertUtf8ByteArrayToString(data, out var utf8str);
+                if (okUtf8) return utf8str;
+
+                bool okLatin1 = Foundation.CharacterSetConverters.TryConvertLatin1ByteArrayToString(data, out var latin1str);
+                if (okLatin1) return latin1str;
+
+                Foundation.CharacterSetConverters.TryConvertASCIIByteArrayToString(data, out var asciistr); // never returns false.
+                return asciistr;
+            }
+
+            /// <summary>
+            /// Converts an IBuffer into a string with either a UTF8 (preferred) or Latin-1 character set.
+            /// </summary>
+            /// <param name="buffer">IBuffer with data</param>
+            /// <returns>non-null string in all cases (even if data is null)</returns>
+            public static string ConvertIBufferToString(IBuffer buffer)
+            {
+                byte[] data = buffer.ToArray();
+                return ConvertByteArrayToString(data);
+            }
+        }
+    }
+}
---
+++ NetworkParsers/UnitTest/TestCharacterSet.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetworkParsers;
+using NetworkParsers.Foundation;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class TestCharacterSet
+    {
+        // Common set of byte arrays to try to convert.
+        readonly byte[] nullbuffer = null;
+        readonly byte[] empty = new byte[] { };
+        readonly byte[] simple = new byte[] { (byte)'a', (byte)'b', (byte)'c' };
+        readonly byte[] bit8 = new byte[] { (byte)'a', 0xA2, (byte)'c' };
+        readonly byte[] bit83 = new byte[] { (byte)'a', 0xA2, 0xA3, 0xA4, (byte)'c' };
+        readonly byte[] embeddednull = new byte[] { (byte)'a', 0x00, 0x00, (byte)'c' };
+        readonly byte[] embeddedc080 = new byte[] { (byte)'a', 0xC0, 0x80, (byte)'c' };
+
+        [TestMethod]
+        public void TestTryConvertASCIIByteArrayToString()
+        {
+            bool ok;
+            string result;
+
+            ok = CharacterSetConverters.TryConvertASCIIByteArrayToString(nullbuffer, out result);
+            Assert.AreEqual(true, ok, "null will convert");
+            Assert.AreEqual("", result, "null converts to empty string");
+
+            ok = CharacterSetConverters.TryConvertASCIIByteArrayToString(empty, out result);
+            Assert.AreEqual(true, ok, "empty will convert");
+            Assert.AreEqual("", result, "empty converts to empty string");
+
+            ok = CharacterSetConverters.TryConvertASCIIByteArrayToString(simple, out result);
+            Assert.AreEqual(true, ok, "simple will convert");
+            Assert.AreEqual("abc", result, "simple converts to abc");
+
+            ok = CharacterSetConverters.TryConvertASCIIByteArrayToString(bit8, out result);
+            Assert.AreEqual(false, ok, "8bit will not convert");
+            Assert.AreEqual("a[A2]c", result, "8bits will convert to...");
+
+            ok = CharacterSetConverters.TryConvertASCIIByteArrayToString(bit83, out result);
+            Assert.AreEqual(false, ok, "83bit will not convert");
+            Assert.AreEqual("a[A2][A3][A4]c", result, "83bits will convert to...");
+
+            ok = CharacterSetConverters.TryConvertASCIIByteArrayToString(embeddednull, out result);
+            Assert.AreEqual(true, ok, "embedded null will convert");
+            Assert.AreEqual("a\0\0c", result, "embedded null will convert to...");
+
+            ok = CharacterSetConverters.TryConvertASCIIByteArrayToString(embeddedc080, out result);
+            Assert.AreEqual(false, ok, "embedded c080 will convert");
+            Assert.AreEqual("a[C0][80]c", result, "embedded c080 will convert to...");
+        }
+
+
+        [TestMethod]
+        public void TestTryConvertLatin1ByteArrayToString()
+        {
+            bool ok;
+            string result;
+
+            ok = CharacterSetConverters.TryConvertLatin1ByteArrayToString(nullbuffer, out result);
+            Assert.AreEqual(true, ok, "null will convert");
+            Assert.AreEqual("", result);
+
+            ok = CharacterSetConverters.TryConvertLatin1ByteArrayToString(empty, out result);
+            Assert.AreEqual(true, ok, "empty will convert");
+            Assert.AreEqual("", result);
+
+            ok = CharacterSetConverters.TryConvertLatin1ByteArrayToString(simple, out result);
+            Assert.AreEqual(true, ok, "simple will convert");
+            Assert.AreEqual("abc", result, "simple will convert to abc");
+
+            ok = CharacterSetConverters.TryConvertLatin1ByteArrayToString(bit8, out result);
+            Assert.AreEqual(true, ok, "8bit will convert");
+            Assert.AreEqual("a¢c", result, "8bit converts--is three chars long!");
+
+            ok = CharacterSetConverters.TryConvertLatin1ByteArrayToString(bit83, out result);
+            Assert.AreEqual(true, ok, "83bit will convert");
+            Assert.AreEqual("a¢£¤c", result, "83bit converts--is three chars long!");
+
+            ok = CharacterSetConverters.TryConvertLatin1ByteArrayToString(embeddednull, out result);
+            Assert.AreEqual(true, ok, "embedded null will convert");
+            Assert.AreEqual("a\0\0c", result, "embedded null will convert");
+
+            ok = CharacterSetConverters.TryConvertLatin1ByteArrayToString(embeddedc080, out result);
+            Assert.AreEqual(true, ok, "embedded c080 will convert");
+            Assert.AreEqual("aÀc", result, "embedded c080 will convert to...");
+
+            // Prove that every single possible byte will convert.
+            for (int i=0; i<256; i++) // can't be a byte indexer
+            {
+                byte[] seq = new byte[] { (byte)i };
+                ok = CharacterSetConverters.TryConvertLatin1ByteArrayToString(seq, out result);
+                Assert.AreEqual(true, ok, $"char {i} will convert will convert");
+                Assert.AreEqual(1, result.Length, $"char {i}.length is 1");
+            }
+        }
+
+        [TestMethod]
+        public void TestTryConvertUtf8ByteArrayToString()
+        {
+            bool ok;
+            string result;
+
+            ok = CharacterSetConverters.TryConvertUtf8ByteArrayToString(nullbuffer, out result);
+            Assert.AreEqual(true, ok, "null will convert");
+            Assert.AreEqual("", result);
+
+            ok = CharacterSetConverters.TryConvertUtf8ByteArrayToString(empty, out result);
+            Assert.AreEqual(true, ok, "empty will convert");
+            Assert.AreEqual("", result);
+
+            ok = CharacterSetConverters.TryConvertUtf8ByteArrayToString(simple, out result);
+            Assert.AreEqual(true, ok, "simple will convert");
+            Assert.AreEqual("abc", result, "simple will convert to abc");
+
+            ok = CharacterSetConverters.TryConvertUtf8ByteArrayToString(bit8, out result);
+            Assert.AreEqual(false, ok, "8bit will convert");
+            Assert.AreEqual(null, result, "8bit converts--is three chars long!");
+
+            ok = CharacterSetConverters.TryConvertUtf8ByteArrayToString(bit83, out result);
+            Assert.AreEqual(false, ok, "83bit will convert");
+            Assert.AreEqual(null, result, "83bit converts--is three chars long!");
+
+            ok = CharacterSetConverters.TryConvertUtf8ByteArrayToString(embeddednull, out result);
+            Assert.AreEqual(true, ok, "embedded null will convert");
+            Assert.AreEqual("a\0\0c", result, "embedded null will convert");
+
+            ok = CharacterSetConverters.TryConvertUtf8ByteArrayToString(embeddedc080, out result);
+            Assert.AreEqual(true, ok, "embedded c080 will convert");
+            //NOTE:
+            // The C080 sequence is an over-long NUL. Normally NUL (8 bits, all zero)
+            // will be encoded in UTF8 as \0, NUL. But in Modified-UTF8 (MUTF-8),
+            // NUL is encoded in an over-long sequence C080 so that the resuting
+            // string doesn't have any NUL chars, ever.
+            // All MUTF-8 readers can also read CESU-8
+            // For now, pin a somewhat unhappy behavior: the overlong sequence
+            // is best-effort converted and the overlong NUL is treated as two
+            // bogus characters and silently replaced.
+            //Assert.AreEqual("a\0c", result, "embedded c080 will convert to...");
+            Assert.AreEqual("a��c", result, "embedded c080 will convert to...");
+        }
+
+        [TestMethod]
+        public void TestTryConvertByteArrayToString()
+        {
+            string result;
+
+            result = CharacterSets.ConvertByteArrayToString(nullbuffer);
+            Assert.AreEqual("", result);
+
+            result = CharacterSets.ConvertByteArrayToString(empty);
+            Assert.AreEqual("", result);
+
+            result = CharacterSets.ConvertByteArrayToString(simple);
+            Assert.AreEqual("abc", result, "simple will convert to abc");
+
+            result = CharacterSets.ConvertByteArrayToString(bit8);
+            Assert.AreEqual("a¢c", result, "8bit converts--is three chars long!");
+
+            result = CharacterSets.ConvertByteArrayToString(bit83);
+            Assert.AreEqual("a¢£¤c", result, "83bit converts--is three chars long!");
+
+            result = CharacterSets.ConvertByteArrayToString(embeddednull);
+            Assert.AreEqual("a\0\0c", result, "embedded null will convert");
+
+            result = CharacterSets.ConvertByteArrayToString(embeddedc080);
+            //NOTE:
+            // The C080 sequence is an over-long NUL. Normally NUL (8 bits, all zero)
+            // will be encoded in UTF8 as \0, NUL. But in Modified-UTF8 (MUTF-8),
+            // NUL is encoded in an over-long sequence C080 so that the resuting
+            // string doesn't have any NUL chars, ever.
+            // All MUTF-8 readers can also read CESU-8
+            // For now, pin a somewhat unhappy behavior: the overlong sequence
+            // is best-effort converted and the overlong NUL is treated as two
+            // bogus characters and silently replaced.
+            //Assert.AreEqual("a\0c", result, "embedded c080 will convert to...");
+            Assert.AreEqual("a��c", result, "embedded c080 will convert to...");
+        }
+
+    }
+}
